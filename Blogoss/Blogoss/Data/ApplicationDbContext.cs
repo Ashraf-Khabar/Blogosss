688f9b74b1cff28b147e7c6fde6191ee954f87b9
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-02-04 01:45:28 +0000

    commit

---
+++ Blogoss/Blogoss/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Blogoss.Models;
+
+namespace Blogoss.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Blogoss.Models.Blog> Blog { get; set; }
+    }
+}
---
+++ Blogoss/Blogoss/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿namespace Blogoss.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public Blog() 
+        {
+            
+        }
+
+
+    }
+}
