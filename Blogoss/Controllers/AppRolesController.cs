6ea1ff41699a04b776524b7fc94389cf8c759370
Achraf Khabar <95541956+Ashraf-Khabar@users.noreply.github.com>
2023-02-10 23:38:52 +0000

    commit

---
+++ Blogoss/Controllers/AppRolesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Blogoss.Controllers
+{
+    public class AppRolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AppRolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+        
+        // List All the Role created by users : 
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> create(IdentityRole model)
+        {
+            // Avoide duplicated role in our application : 
+            if(!_roleManager.RoleExistsAsync(model.Name).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(model.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- Blogoss/Program.cs
+++ Blogoss/Program.cs
@@ -10,7 +10,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>().AddDefaultTokenProviders()
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
